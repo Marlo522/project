83eaac1779992ebd2abc02c8dfbeec4953159eb8
Marlo22 <veluzmarlo959@gmail.com>
2025-12-28 11:06:46 +0800

    feat: Add Book model with mass assignable attributes

---
+++ project/app/Models/Book.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+class Book extends Model
+{
+    /** @use HasFactory<\Database\Factories\BookFactory> */
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'author',
+        'published_year',
+        'genre',
+        'quantity',
+    ];
+
+    
+
+    
+}
