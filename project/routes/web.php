cb030e211d48e1bba57b8dfc0d9d418336eab473
Marlo22 <veluzmarlo959@gmail.com>
2025-12-28 15:55:27 +0800

    feat: Add CRUD routes for managing books in web.php

--- project/routes/web.php
+++ project/routes/web.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\BookController;
+use Illuminate\Support\Facades\Route;
 
 
 
@@ -27,6 +29,22 @@ Route::get('/', function () {
     return view('home');
 })->middleware('auth');
 
-Route::get('/users', [UserController::class, 'index'])
-    ->middleware('auth')
-    ->name('users.index');
+// add user routes
+
+Route::middleware('auth')->group(function () {
+    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+    Route::post('/users', [UserController::class, 'store'])->name('users.store');
+    Route::get('/users', [UserController::class, 'index'])->name('users.index');
+    Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
+    Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
+    Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/books/create', [BookController::class, 'create'])->name('books.create');
+    Route::post('/books', [BookController::class, 'store'])->name('books.store');
+    Route::get('/books', [BookController::class, 'index'])->name('books.index');
+    Route::get('/books/{book}/edit', [BookController::class, 'edit'])->name('books.edit');
+    Route::put('/books/{book}', [BookController::class, 'update'])->name('books.update');
+    Route::delete('/books/{book}', [BookController::class, 'destroy'])->name('books.destroy');
+});
